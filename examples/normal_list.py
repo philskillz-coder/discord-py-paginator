e9defc239711f4e2a8695ed22474c91f01fc2fa0
philskillz-coder <76884393+philskillz-coder@users.noreply.github.com>
2022-03-27 13:08:38 +0200

    Create normal_list.py

---
+++ examples/normal_list.py
@@ -0,0 +1,33 @@
+from discord import app_commands, Embed, Color, Interaction
+from discord.ext.paginator import paginator
+from typing import Dict, Any
+
+class ColorPaginator(paginator.Paginator):
+    pass
+    
+@app_commands.command(
+    name="colors",
+    description="Show some colors"
+)
+async def show_colors(interaction: Interaction):
+    await interaction.response.send_message(
+        content="Nice colors",
+        view=await ColorPaginator.from_list(
+            interaction.client,
+            interaction.user,
+            data=[
+                {
+                    "content": "The color red",
+                    "embed": Embed(title="Red", color=Color.red()),
+                },
+                {
+                    "content": "The color green",
+                    "embed": Embed(title="Green", color=Color.green()),
+                },
+                {
+                    "content": "The color blue",
+                    "embed": Embed(title="Blue", color=Color.blue()),
+                }
+            ]
+        ).run()
+    )
