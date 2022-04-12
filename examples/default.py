592ec772b692e1d134069f81c7d573a8871a057d
philskillz-coder <76884393+philskillz-coder@users.noreply.github.com>
2022-04-12 15:31:20 +0200

    update examples

--- examples/default.py
+++ examples/default.py
@@ -3,18 +3,18 @@ from discord.ext.paginator import paginator
 from typing import Dict, Any
 
 class GuildPaginator(paginator.Paginator):
-    async def get_page_count(self) -> int:
+    async def get_page_count(self, interaction: Interaction) -> int:
         return len(self.client.guilds)
 
     async def get_page_content(self, interaction: Interaction, page: int) -> Dict[str, Any]:
         # this method should return the arguments used for interaction.response.edit_message
         # e.g. {'content': 'hello'} means the message content will be edited to hello
 
-        guild = self.client.guilds[page - 1]
+        guild = self.client.guilds[page]
         # this cannot throw a index error because page is between 0 and the guild count
 
         return {
-            "content": f"Guild {page}/{await self.get_page_count(interaction)}",
+            "content": f"Guild {page+1}/{await self.get_page_count(interaction)}",
             "embed": (
                 Embed(
                     title="Guild",
