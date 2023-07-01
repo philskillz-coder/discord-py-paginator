ac6d394e3d810c61bafa43b5467807265bca9b4e
Philskillz <76884393+philskillz-coder@users.noreply.github.com>
2023-07-01 13:39:39 +0200

    updates

--- examples/send_paginator.py
+++ examples/send_paginator.py
@@ -15,35 +15,12 @@ bot = commands.Bot(
 
 
 class MemberPaginator(Paginator):
-    paginator_delete_when_finished = True
-    paginator_delete_delay = 0  # 0 seconds
-    paginator_not_allowed_message = "Sorry, its not for you!"
-
-    page_number_button_label = "%s/%s"
-
     def __init__(self, client: commands.Bot, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.client = client
 
     async def get_page_count(self, interaction: discord.Interaction) -> int:
         return interaction.guild.member_count
-
-    async def page_update(self, interaction: discord.Interaction, current_page: int):
-        member = interaction.guild.members[current_page]
-
-        return await interaction.response.edit_message(
-            content=f"Member {current_page + 1}/{await self.get_page_count(interaction)}",
-            embed=(
-                discord.Embed(
-                    title=f"Member {member}"
-                )
-                .add_field(name="Created at", value=str(member.created_at), inline=False)
-                .add_field(name="Joined at", value=str(member.joined_at), inline=False)
-            ),
-            view=self
-        )
-
-    search_button_error_message = "Nothing found for '%s'"
 
     async def search_page(self, interaction: discord.Interaction, query: str) -> Optional[int]:
         for i, member in enumerate(interaction.guild.members):
@@ -53,24 +30,23 @@ class MemberPaginator(Paginator):
 
         return None
 
-    async def on_setup(self, *args, **kwargs):
-        # This is called by Paginator.run (when the paginator is sent)
-        # You could set up a database connection here, for example.
-        # But I would rather do that in on_start because you're wasting resources if the paginator is not started.
+    async def page_update(self, interaction: discord.Interaction, current_page: int):
+        member = interaction.guild.members[current_page]
 
-        print("Setup")
-
-    async def on_start(self, interaction: discord.Interaction):
-        # This is called when the paginator is started
-        # If you need a database connection, you should set it up here.
-
-        print("Start")
-
-    async def on_stop(self, interaction: discord.Interaction):
-        # This is called when the paginator is stopped with the stop button NOT WHEN IT TIMES OUT
-        # If you needed a database connection, you should close it here.
-
-        print("Stop")
+        return await interaction.response.edit_message(
+            content=f"Member {current_page + 1}/{await self.get_page_count(interaction)}",
+            embed=(
+                discord.Embed(
+                    title=str(member),
+                    colour=discord.Color.green()
+                )
+                .add_field(name="ID", value=str(member.id))
+                .add_field(name="Created at", value=str(member.created_at), inline=False)
+                .add_field(name="Joined at", value=str(member.joined_at), inline=False)
+                .add_field(name="Roles", value=", ".join([role.name for role in member.roles]))
+            ),
+            view=self
+        )
 
 
 @bot.command()
@@ -130,8 +106,7 @@ async def show_members(interaction: discord.Interaction):
         view=await MemberPaginator(
             interaction.client,
             interaction.user
-        ).run(),
-        ephemeral=True
+        ).run()
     )
 
 
